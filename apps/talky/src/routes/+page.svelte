5abfdcf5f5e5017d39586fd071d84f2b6791dda6
Tim Feid <tim@timfeid.com>
2025-04-06 11:12:09 -0400

    fix: one thing

--- apps/talky/src/routes/+page.svelte
+++ apps/talky/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-	import Layout from '$lib/layout/layout.svelte';
 	import { user } from '$lib/user.svelte';
 	import { onMount } from 'svelte';
 
@@ -9,15 +8,13 @@
 	});
 </script>
 
-<Layout>
-	<div class="p-3">
-		<div class="p-3">welcome</div>
-		<div>
-			{#if user.user}
-				<Button href="/of/devils/gameday">go to the channel?</Button>
-			{:else}
-				<Button href="/login">Log in</Button>
-			{/if}
-		</div>
+<div class="p-3">
+	<div class="p-3">welcome</div>
+	<div>
+		{#if user.user}
+			<Button href="/of/devils/gameday">go to the channel?</Button>
+		{:else}
+			<Button href="/login">Log in</Button>
+		{/if}
 	</div>
-</Layout>
+</div>
